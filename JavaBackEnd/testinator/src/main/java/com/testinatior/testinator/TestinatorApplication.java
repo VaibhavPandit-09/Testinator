89fb3e6a196a62186d6b19050d3d2b044f0654ef
Vaibhav <vaibhavpandit39@gmail.com>
2024-03-21 21:56:48 +0530

    first commit

---
+++ JavaBackEnd/testinator/src/main/java/com/testinatior/testinator/TestinatorApplication.java
@@ -0,0 +1,13 @@
+package com.testinatior.testinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestinatorApplication.class, args);
+	}
+
+}
---
+++ JavaBackEnd/testinator/src/test/java/com/testinatior/testinator/TestinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testinatior.testinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
